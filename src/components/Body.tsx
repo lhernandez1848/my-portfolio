8348421c72db2a9c1edd84385edd48ec914055be
lhernandez1848 <lisdanayhernandez@outlook.com>
2025-08-10 14:34:30 -0400

    PSSP-09: tailwind v3

---
+++ src/components/Body.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import "../css/global.scss";
+
+export default function Body({ children }: { children: React.ReactNode }) {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleChange = () => {
+    localStorage.setItem('isDark', (!isDark).toString());
+    setIsDark(!isDark);
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsDark(localStorage.getItem('isDark') === 'true');
+    }
+  }, [])
+
+  return (
+    <body data-theme={isDark ? 'dark' : 'light'} className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-no-repeat">
+      <Navbar isDark={isDark} handleChange={handleChange} />
+      {children}
+    </body>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['selector', '[data-theme="dark"]'],
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
